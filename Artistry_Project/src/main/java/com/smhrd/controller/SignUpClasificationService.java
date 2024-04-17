6cea70ed0064cdc02d518f2f245fb335b17cb601
이성도 <dltjdeh7745@naver.com>
2024-04-17 17:22:51 +0900

    테스트

---
+++ Artistry_Project/src/main/java/com/smhrd/controller/SignUpClasificationService.java
@@ -0,0 +1,22 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignUpClasificationService extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		
+		String checkbox1 = request.getParameter("checkbox1");
+		String checkbox2 = request.getParameter("checkbox2");
+		
+		System.out.println("checkbox1 : " + checkbox1);
+		System.out.println("checkbox2 : " + checkbox2);
+	
+	}
+
+}
