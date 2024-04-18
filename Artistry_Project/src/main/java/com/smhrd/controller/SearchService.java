4ab8d9eb1a0d484d729024a488c1b47b0fc03d0f
smhrd <smhrd@61.80.80.252>
2024-04-18 16:16:33 +0900

    search 조준범

--- Artistry_Project/src/main/java/com/smhrd/controller/SearchService.java
+++ Artistry_Project/src/main/java/com/smhrd/controller/SearchService.java
@@ -8,7 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SearchService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("test");
+		String keyWord = request.getParameter("Search-2");
+		
+		response.sendRedirect("artist-portfolio/portfolio.jsp?keyWord="+keyWord);
+		
 	}
 
 }
